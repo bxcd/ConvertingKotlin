323669d08362ee16893d3276d6d4c0f7d2fddf90
rjbx <bob@coded.art>
2022-01-09 02:54:16 -0500

    Apply Kotlin code design principles to correct Repository class converted logic

--- app/src/main/java/art/coded/convertingkotlin/Repository.kt
+++ app/src/main/java/art/coded/convertingkotlin/Repository.kt
@@ -1,61 +1,39 @@
 package art.coded.convertingkotlin
 
-import art.coded.convertingkotlin.User.lastName
-import art.coded.convertingkotlin.User.firstName
-import java.util.ArrayList
-
-class Repository private constructor() {
-    private var users: MutableList<User?>? = null
-    fun getUsers(): List<User?>? {
-        return users
-    }
-
-    val formattedUserNames: List<String?>
-        get() {
-            val userNames: MutableList<String?> = ArrayList(
-                users!!.size
-            )
-            for (user in users) {
-                var name: String
-                name = if (user!!.lastName != null) {
-                    if (user!!.firstName != null) {
-                        user!!.firstName + " " + user!!.lastName
-                    } else {
-                        user!!.lastName
-                    }
-                } else if (user!!.firstName != null) {
-                    user!!.firstName
-                } else {
-                    "Unknown"
-                }
-                userNames.add(name)
+val User.formattedName: String
+    get() {
+         return if (lastName != null) {
+            if (firstName != null) {
+                "$firstName $lastName"
+            } else {
+                lastName ?: "Unknown"
             }
-            return userNames
+        } else {
+            firstName ?: "Unknown"
         }
-
-    companion object {
-        private var INSTANCE: Repository? = null
-        val instance: Repository?
-            get() {
-                if (INSTANCE == null) {
-                    synchronized(Repository::class.java) {
-                        if (INSTANCE == null) {
-                            INSTANCE = Repository()
-                        }
-                    }
-                }
-                return INSTANCE
-            }
     }
+
+object Repository {
+
+    private val _users = mutableListOf<User>()
+    val users: List<User>
+        get() = _users
+
+    val formattedUserNames: List<String>
+        get() {
+            return users.map { user -> user.formattedName }
+        }
 
     // keeping the constructor private to enforce the usage of getInstance
     init {
         val user1 = User("Jane", "")
         val user2 = User("John", null)
         val user3 = User("Anne", "Doe")
-        users = ArrayList<Any?>()
-        users.add(user1)
-        users.add(user2)
-        users.add(user3)
+
+        _users.apply {
+            add(user1)
+            add(user2)
+            add(user3)
+        }
     }
 }
